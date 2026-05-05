ae8668347c5ec98753a9567f6020251d721e3ff7
Katarina Korizma <katarina.korizma@gmail.com>
2026-05-05 18:08:11 +0200

    Protect modifying API routes with Sanctum

--- web-edukacija/routes/api.php
+++ web-edukacija/routes/api.php
@@ -19,19 +19,65 @@ use App\Http\Controllers\Api\AuthController;
 |
 */
 
-// Authentication routes
+// Public authentication routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// Extra specific routes (must be defined before resource routes)
-Route::get('/rooms/available', [RoomController::class, 'available']);
+// Public GET routes for hotels
+Route::get('/hotels', [HotelController::class, 'index']);
+Route::get('/hotels/{hotel}', [HotelController::class, 'show']);
 Route::get('/hotels/{hotel}/rooms', [HotelController::class, 'rooms']);
-Route::get('/reservations/user/{user}', [ReservationController::class, 'userReservations']);
 
-// API Resource routes
-Route::apiResource('hotels', HotelController::class);
-Route::apiResource('room-types', RoomTypeController::class);
-Route::apiResource('rooms', RoomController::class);
-Route::apiResource('reservations', ReservationController::class);
-Route::apiResource('payments', PaymentController::class);
+// Public GET routes for room types
+Route::get('/room-types', [RoomTypeController::class, 'index']);
+Route::get('/room-types/{roomType}', [RoomTypeController::class, 'show']);
+
+// Public GET routes for rooms (specific routes first)
+Route::get('/rooms', [RoomController::class, 'index']);
+Route::get('/rooms/available', [RoomController::class, 'available']);
+Route::get('/rooms/{room}', [RoomController::class, 'show']);
+
+// Public GET routes for reservations (specific routes first)
+Route::get('/reservations', [ReservationController::class, 'index']);
+Route::get('/reservations/user/{user}', [ReservationController::class, 'userReservations']);
+Route::get('/reservations/{reservation}', [ReservationController::class, 'show']);
+
+// Public GET routes for payments
+Route::get('/payments', [PaymentController::class, 'index']);
+Route::get('/payments/{payment}', [PaymentController::class, 'show']);
+
+// Protected routes requiring authentication
+Route::middleware('auth:sanctum')->group(function () {
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Hotels (POST, PUT/PATCH, DELETE)
+    Route::post('/hotels', [HotelController::class, 'store']);
+    Route::put('/hotels/{hotel}', [HotelController::class, 'update']);
+    Route::patch('/hotels/{hotel}', [HotelController::class, 'update']);
+    Route::delete('/hotels/{hotel}', [HotelController::class, 'destroy']);
+    
+    // Room Types (POST, PUT/PATCH, DELETE)
+    Route::post('/room-types', [RoomTypeController::class, 'store']);
+    Route::put('/room-types/{roomType}', [RoomTypeController::class, 'update']);
+    Route::patch('/room-types/{roomType}', [RoomTypeController::class, 'update']);
+    Route::delete('/room-types/{roomType}', [RoomTypeController::class, 'destroy']);
+    
+    // Rooms (POST, PUT/PATCH, DELETE)
+    Route::post('/rooms', [RoomController::class, 'store']);
+    Route::put('/rooms/{room}', [RoomController::class, 'update']);
+    Route::patch('/rooms/{room}', [RoomController::class, 'update']);
+    Route::delete('/rooms/{room}', [RoomController::class, 'destroy']);
+    
+    // Reservations (POST, PUT/PATCH, DELETE)
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::put('/reservations/{reservation}', [ReservationController::class, 'update']);
+    Route::patch('/reservations/{reservation}', [ReservationController::class, 'update']);
+    Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy']);
+    
+    // Payments (POST, PUT/PATCH, DELETE)
+    Route::post('/payments', [PaymentController::class, 'store']);
+    Route::put('/payments/{payment}', [PaymentController::class, 'update']);
+    Route::patch('/payments/{payment}', [PaymentController::class, 'update']);
+    Route::delete('/payments/{payment}', [PaymentController::class, 'destroy']);
+});
